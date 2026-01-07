98646e05bdf31ff5e4603fa578dbf82c3a09301f
Yahya Dai B <154234185+Kyrieligh@users.noreply.github.com>
2026-01-07 16:20:09 +0700

    Add files via upload

---
+++ Modul-5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 5 Pemrograman Web</title>
+</head>
+
+<body>
+    <?php 
+        $nim = "2305101098";
+        $nama = "Rizky Muhammad Ristya";
+        $angka1 = 20;
+        $angka2 = 180;
+
+        $hasilpenjumlahan = $angka1 + $angka2;
+
+        echo "NIM : ". $nim . "<br/>";
+        echo "NAMA : ". $nama . "<br/>";
+        echo "Hasil penjumlahan dari ". $angka1 . " dan " . $angka2 . " = " . $hasilpenjumlahan . "<br/>";
+    ?>
+</body>
+
+</html>
